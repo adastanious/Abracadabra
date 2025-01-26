6aad27ead7d5803ebc47aadf998eb2f1bab38dfd
Aiperi <aibige24wka@gmail.com>
2025-01-26 13:58:09 +0600

    Aiperi

--- src/main/java/talentLMS/page/UsersPage.java
+++ src/main/java/talentLMS/page/UsersPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import talentLMS.entity.User;
 
-
 public class UsersPage extends BasePage {
 
     // xpath для поля firstName
@@ -35,8 +34,36 @@ public class UsersPage extends BasePage {
     @FindBy(xpath = "//input[@name='submit_personal_details']")
     public WebElement addUserButton;
 
+    // xPath для клика users
+    @FindBy(xpath = "(//span[@class='tl-box-title-options']/a)[2]")
+    public WebElement users;
 
-    public UsersPage addNewUser(User user){
+    //xpath поля name в разделе редактирования
+    @FindBy(xpath = "//td[@class=' tl-align-left hidden-phone']/span[@title='Learner-Type']")
+    public WebElement learnerType;
+
+    // xpath кнопки edit
+    @FindBy(xpath = "(//div/i[@class='icon-pencil icon-grid' and @alt='Edit'])[2]")
+    public WebElement edit;
+
+    // xpath кнопки поля first name в разделе edit
+    @FindBy(xpath = "//div[@class='input-append tl-countdown']/input[@type='text' and @name='name']")
+    public WebElement clickEditName;
+
+    //xpath кнопки submit
+    @FindBy(xpath = "//input[@name='submit_personal_details']")
+    public WebElement submit;
+
+    // xPath значок troetochie
+    @FindBy(xpath = "((//td[@class=' tl-align-center tl-table-operations-cell']/div[@class='tl-table-operations-trigger touchable'])[2])")
+    public WebElement operationIcon;
+
+    // xPath удалить пользователя
+    @FindBy(xpath = "//a[@id='tl-confirm-submit']")
+    public WebElement deleteClick;
+
+    // метод для добавления нового пользователя
+    public UsersPage addNewUser(User user) {
         webElementActions.waitElementToBeDisplayed(this.firstName);
         webElementActions.sendKeys(this.firstName, user.getFirstname())
                 .sendKeys(this.lastName, user.getLastname())
@@ -47,8 +74,8 @@ public class UsersPage extends BasePage {
                 .click(addUserButton);
         return this;
     }
-
-    public UsersPage addNewUserNoCorrect(String firstName, String lastName, String email, String login, String password, String bioDescription){
+    // проверка негативного кейса(email)
+    public UsersPage addNewUserNoCorrect(String firstName, String lastName, String email, String login, String password, String bioDescription) {
         webElementActions.waitElementToBeDisplayed(this.firstName);
         webElementActions.sendKeys(this.firstName, firstName)
                 .sendKeys(this.lastName, lastName)
@@ -60,6 +87,28 @@ public class UsersPage extends BasePage {
         return this;
     }
 
-
+    // метод редактирования пользователя
+    public UsersPage editUserName(User user) {
+        webElementActions.click(learnerType)
+                .click(edit)
+                .click(clickEditName)
+                .sendKeys(clickEditName, user.getFirstname())
+                .click(submit);
+        return this;
     }
+    // метод удаления пользователя
+    public UsersPage deleteUsers(User user) {
+        webElementActions.click(operationIcon)
+                .click(deleteClick);
+        return this;
+    }
+}
+
+
+
+
+
+
+
+
 
