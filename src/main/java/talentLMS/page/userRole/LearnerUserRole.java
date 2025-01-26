191b4d94e5ad00fdfbe6f61c736a675889469d4d
Agema <zamirbekova.aigerim96@gmail.com>
2025-01-26 17:12:41 +0600

    agema

--- src/main/java/talentLMS/page/userRole/LearnerUserRole.java
+++ src/main/java/talentLMS/page/userRole/LearnerUserRole.java
@@ -7,6 +7,30 @@ public class LearnerUserRole extends UserRole{
     @FindBy(xpath = "(//i[@class='tl-select-icon' ])[2]")
     public WebElement learnerUserRole;
 
+    // Поля для некликабельных элементов
+    @FindBy(xpath = "//div[@title='Show courses in progress']")
+    public WebElement coursesInProgress;
+
+    @FindBy(xpath = "//div[@class='item-caption' and text()='Completed courses']")
+    public WebElement completedCourses;
+
+    @FindBy(xpath = "//div[@class='item-caption' and text()='Training time']")
+    public WebElement trainingTime;
+
+    @FindBy(xpath = "//div[@title='Badges']")
+    public WebElement badges;
+
+    @FindBy(xpath = "//div[@title='Points leaderboard']")
+    public WebElement points;
+
+    // Курсы, доступные для Learner
+    @FindBy(xpath = "//span[@title='[Edit me] Guide for Learners (001)']")
+    public WebElement guideForLearnersCourse;
+
+    @FindBy(xpath = "//span[@title='What is TalentLibrary? (002)']")
+    public WebElement talentLibraryCourse;
+
+
     public void clickLearnerUserRole(){
         webElementActions.click(learnerUserRole);
     }
--- src/test/java/LearnerUserRoleTest.java
+++ src/test/java/LearnerUserRoleTest.java
@@ -1,9 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import talentLMS.page.userRole.LearnerUserRole;
 
 public class LearnerUserRoleTest extends BaseTest{
-@Test
-    public void learnerUserRoleTest(){
-    learnerUserRole.moveUserRole();
-    learnerUserRole.clickLearnerUserRole();
-}
+
+    @Test
+    public void learnerUserRoleTest() {
+        LearnerUserRole learnerUserRole = new LearnerUserRole();
+        learnerUserRole.moveUserRole();
+        learnerUserRole.clickLearnerUserRole();
+
+        // Проверка некликабельных элементов (они должны отображаться, но не быть активными)
+        Assert.assertTrue(learnerUserRole.coursesInProgress.isDisplayed(), "Courses in progress не отображается!");
+        Assert.assertTrue(learnerUserRole.completedCourses.isDisplayed(), "Completed courses не отображается!");
+        Assert.assertTrue(learnerUserRole.trainingTime.isDisplayed(), "Training time не отображается!");
+        Assert.assertTrue(learnerUserRole.badges.isDisplayed(), "Badges не отображается!");
+        Assert.assertTrue(learnerUserRole.points.isDisplayed(), "Points leaderboard не отображается!");
+
+        //  Проверка доступных курсов
+        Assert.assertTrue(learnerUserRole.guideForLearnersCourse.isDisplayed(), "Курс '[Edit me] Guide for Learners' отсутствует!");
+        Assert.assertTrue(learnerUserRole.talentLibraryCourse.isDisplayed(), "Курс 'What is TalentLibrary?' отсутствует!");
+
+
+    }
+
 }
