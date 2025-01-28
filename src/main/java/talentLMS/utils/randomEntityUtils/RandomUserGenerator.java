f874fe2155ab38fd6fc17b6f6b025a57173a2168
Nasyikat <Nasi_94@mail.ru>
2025-01-28 21:12:24 +0600

    Nasyikat_Users

--- src/main/java/talentLMS/utils/randomEntityUtils/RandomUserGenerator.java
+++ src/main/java/talentLMS/utils/randomEntityUtils/RandomUserGenerator.java
@@ -1,11 +1,11 @@
 package talentLMS.utils.randomEntityUtils;
 
 import com.github.javafaker.Faker;
-import talentMLS.entity.User;
+import talentLMS.entity.User;
 
-import java.util.Random;
 
 public class RandomUserGenerator {
+
     Faker faker = new Faker();
 
     public String randomFirstName() {
@@ -24,7 +24,6 @@ public class RandomUserGenerator {
         return faker.name().username();
     }
 
-    // метод для генерации корректного пароля
     public String randomPassword() {
         String password;
         do {
@@ -34,25 +33,19 @@ public class RandomUserGenerator {
     }
 
     private boolean isValidPassword(String password) {
-        // Исправленное регулярное выражение
         return password.matches("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}");
     }
 
     public User randomUser(){
-        return new User(randomFirstName(), randomLastName(), randomEmail(), randomUserName(), randomPassword(), generateText(100));
+        return new User(randomFirstName(), randomLastName(), randomEmail(), randomUserName(), randomPassword());
     }
 
-    // Метод для генерации случайного текста длиной до 800 символов
-    public String generateText(int length) {
-        StringBuilder sb = new StringBuilder();  // Строка, в которую будем добавлять символы
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?/";  // Набор символов
+    public User randomUserWithIncorrectEmail(){
+        return new User(randomFirstName(), randomLastName(), "Alisa.com", randomUserName(), randomPassword());
+    }
 
-        Random random = new Random();
-        for (int i = 0; i < length; i++) {
-            int randomIndex = random.nextInt(characters.length());  // Выбираем случайный индекс из набора
-            sb.append(characters.charAt(randomIndex));  // Добавляем выбранный символ в строку
-        }
-        return sb.toString();  // Возвращаем сгенерированный текст
+    public User emailUniquenessCheck(){
+        return new User(randomFirstName(), randomLastName(), "das.digital.nomads@gmail.com", randomUserName(), randomPassword());
     }
 
 }
--- src/test/java/talentLMS/BaseTest.java
+++ src/test/java/talentLMS/BaseTest.java
@@ -3,8 +3,8 @@ package talentLMS;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeSuite;
 import talentLMS.driver.Driver;
+import talentLMS.entity.User;
 import talentLMS.page.users.UserPage;
-import talentMLS.entity.User;
 import talentLMS.entity.Sections;
 import talentLMS.helper.WebElementActions;
 import talentLMS.page.login.LoginPage;
@@ -13,12 +13,12 @@ import talentLMS.utils.randomEntityUtils.RandomUserGenerator;
 public abstract class BaseTest {
     public WebDriver driver;
 
-    WebElementActions webElementActions = new WebElementActions();
+    public WebElementActions webElementActions = new WebElementActions();
     LoginPage loginPage = new LoginPage();
     Sections sections = new Sections();
-    UserPage userPage = new UserPage();
+    public UserPage userPage = new UserPage();
     public RandomUserGenerator randomUserGenerator = new RandomUserGenerator();
-    User randomUser = randomUserGenerator.randomUser();
+    public User randomUser = randomUserGenerator.randomUser();
 
     @BeforeSuite
     public void beforeSuite() {
