2eed9f60ada30856def7b0d97590a09a090954f0
Agema <zamirbekova.aigerim96@gmail.com>
2025-02-10 10:02:40 +0600

    случайно зацепила idea и снова убрала

--- src/main/java/talentLMS/enums/ErrorMessage.java
+++ src/main/java/talentLMS/enums/ErrorMessage.java
@@ -6,10 +6,13 @@ import lombok.Getter;
 public enum ErrorMessage{
     CATEGORIES_ADD_NAME_LENGTH_MORE80("'Name' cannot exceed 80 characters"),
     CATEGORIES_PRICE_MESSAGE("This is not a valid 'Price'"),
-    CATEGORIES_ADD_NAME_LENGTH_LESS1("'Name' is required")
+    CATEGORIES_ADD_NAME_LENGTH_LESS1("'Name' is required"),
 
-    ;
-    private String message;
+    FILE_TYPE_NOT_ALLOWED("File type not allowed"),  // Ошибка при загрузке неподдерживаемого файла
+    IMPORT_BUTTON_DISABLED("Import button should be disabled without a file"),  // Ошибка, если кнопка активна без файла
+    IMPORT_BUTTON_DISABLED_DURING_UPLOAD("Import button should be disabled during file upload");  // Ошибка, если кнопка активна во время загрузки
+
+    private final String message;
 
     ErrorMessage(String message) {
         this.message = message;
